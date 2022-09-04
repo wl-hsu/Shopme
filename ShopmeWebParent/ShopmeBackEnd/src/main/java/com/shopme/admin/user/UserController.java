01f2ad8c64cec6fbe1f47e3874510d286e403766
wl-hsu <wlhsu.github@gmail.com>
2022-09-04 13:52:41 -0400

    add create admin user form and function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 import java.util.List;
+
+
 
 @Controller
 public class UserController {
@@ -22,5 +27,23 @@ public class UserController {
 		return "users";
 	}
 	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		User user = new User();
+		user.setEnabled(true);
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
+	}
 	
 }

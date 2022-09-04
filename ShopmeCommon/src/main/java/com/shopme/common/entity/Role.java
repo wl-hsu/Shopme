6c1e4abde5f988244b16f8335f47f56aac31ff6c
wl-hsu <wlhsu.github@gmail.com>
2022-09-03 21:45:45 -0400

    add User Entity, Repository, and Unit Tests

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -1,5 +1,7 @@
 package com.shopme.common.entity;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,6 +28,14 @@ public class Role {
 	public Role() {
 	}
 	
+	
+	
+	public Role(Integer id) {
+		this.id = id;
+	}
+
+
+
 	public Role(String name) {
 		this.name = name;
 	}
@@ -58,5 +68,46 @@ public class Role {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Role [name=" + name + "]";
+	}
+
+	
+
+	
+	
 
 }
---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -0,0 +1,97 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUserwithOneRole() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User userWLH = new User("test@test.com", "passwordtest", "WL", "H");
+		userWLH.addRole(roleAdmin);
+		
+		User savedUser = repo.save(userWLH);
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateUserwithTwoRoles() {
+		User userWLH2 = new User("test2@test.com", "passwordtest", "WL2", "H2");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		
+		userWLH2.addRole(roleEditor);
+		userWLH2.addRole(roleAssistant);
+		
+		User savedUser = repo.save(userWLH2);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
+		
+	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+	}
+	
+	@Test
+	public void testGetUserById() {
+		User userWLH = repo.findById(1).get();
+		System.out.println(userWLH);
+		assertThat(userWLH).isNotNull();
+	}
+	
+	@Test
+	public void testUpdateUserDetails() {
+		User userWLH = repo.findById(1).get();
+		userWLH.setEnabled(true);
+		userWLH.setEmail("updateDeatil@gmail.com");
+		
+		repo.save(userWLH);
+	}
+	
+	@Test
+	public void testUpdateUserRoles() {
+		User userWLH2 = repo.findById(2).get();
+		Role roleEditor = new Role(3);
+		Role roleSalesperson = new Role(2);
+		
+		userWLH2.getRoles().remove(roleEditor);
+		userWLH2.addRole(roleSalesperson);
+		
+		repo.save(userWLH2);
+	}
+	
+	@Test
+	public void testDeleteUser() {
+		Integer userId = 2;
+		repo.deleteById(userId);
+		
+	}
+	
+}
